d113e1e253f53060e47c526a90af103de12ae536
Geovanna Brandão <52009569+favillc@users.noreply.github.com>
2021-01-15 18:15:13 -0300

    Kata de 7 Kyu

---
+++ findthestraynumber.js
@@ -0,0 +1,12 @@
+function stray(numbers) {
+  for(let i=0; i<numbers.length; i++){
+      if(numbers[i] === numbers[i+1]){
+          if(numbers[i] === numbers[i+2]){
+          } else {
+            return numbers[i+2];
+          }
+      } else if(numbers[i] !== numbers[i+2]){
+      return numbers[i]
+      }
+  }
+  }
