c5a853d09f8db9dc71e841f0c72cb348f3784424
Geovanna Brandão <52009569+favillc@users.noreply.github.com>
2021-01-15 18:09:41 -0300

    Kata de 8 Kyu

---
+++ switchup.js
@@ -0,0 +1,37 @@
+function switchItUp(number){
+let arr = ['Zero','One','Two','Three', 'Four','Five','Six','Seven','Eight','Nine'];
+  switch(number) {
+    case 0:
+      return arr[number];
+      break;
+    case 1:
+      return arr[number];
+      break;
+    case 2:
+      return arr[number];
+      break;
+    case 3:
+      return arr[number];
+      break;
+    case 4:
+      return arr[number];
+      break;
+    case 5:
+      return arr[number];
+      break;
+    case 6:
+      return arr[number];
+      break;
+    case 7:
+      return arr[number];
+      break;
+    case 8:
+      return arr[number];
+      break;
+    case 9:
+      return arr[number];
+      break;
+  default:
+    // code block
+  }
+}
